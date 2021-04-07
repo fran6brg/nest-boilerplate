594fbc93aa00fd964135d27c4e8e0bad6af1c450
Francis Berger <francis@Imac-de-Francis.local>
2021-04-07 19:46:53 +0200

    ok

--- backend-mongoose/src/users/users.service.ts
+++ backend-mongoose/src/users/users.service.ts
@@ -8,60 +8,60 @@ import { NewUserInput } from './dto/new-user.input';
 
 @Injectable()
 export class UsersService {
-    // Once you've registered the schema, you can inject a model into the service using the @InjectModel() decorator
-    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  // Once you've registered the schema, you can inject a model into the service using the @InjectModel() decorator
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-    /**
-     * 
-     */
-    async findAll(): Promise<User[]> {
-        return await this.userModel.find().exec();
+  /**
+   *
+   */
+  async findAll(): Promise<User[]> {
+    return await this.userModel.find().exec();
+  }
+
+  /**
+   *
+   * @param username
+   */
+  async findOne(username: string): Promise<User | undefined> {
+    console.log('users.service | findOne');
+    const users = await this.userModel.find().exec();
+    const user = users.find((user) => user.username === username);
+    // console.log("                >>> users.length:", users.length, "| user:", user);
+    return user;
+  }
+
+  /**
+   *
+   * @param username
+   */
+  async findByUsername(username: string): Promise<User | undefined> {
+    try {
+      console.log('users.service | findByUsername');
+      const user = await this.userModel.findOne({ username }).exec();
+
+      if (!user)
+        throw new HttpException(
+          `User with username ${username} does not exist`,
+          HttpStatus.NOT_FOUND,
+        );
+
+      return user;
+    } catch (error) {
+      console.log('error:', error);
     }
+  }
 
-    /**
-     *
-     * @param username
-     */
-    async findOne(username: string): Promise<User | undefined> {
-        console.log("users.service | findOne");
-        const users = await this.userModel.find().exec();
-        const user = users.find(user => user.username === username);
-        // console.log("                >>> users.length:", users.length, "| user:", user);
-        return user;
+  /**
+   *
+   * @param createUserDTO
+   */
+  async create(createUserDTO: NewUserInput): Promise<User> {
+    try {
+      const createdUser = new this.userModel(createUserDTO);
+      await createdUser.save();
+      return createdUser;
+    } catch (error) {
+      console.log('users.service | error:', error);
     }
-
-    /**
-     * 
-     * @param username 
-     */
-    async findByUsername(username: string): Promise<User | undefined> {
-        try {
-            console.log("users.service | findByUsername");
-            const user = await this.userModel.findOne({username}).exec();
-
-            if (!user)
-                throw new HttpException(`User with username ${username} does not exist`, HttpStatus.NOT_FOUND);
-
-            return user;
-        }
-        catch (error) {
-            console.log("error:", error);
-        }
-    }
-
-    /**
-     * 
-     * @param createUserDTO 
-     */
-    async create(createUserDTO: NewUserInput): Promise<User> {
-        try {
-            const createdUser = new this.userModel(createUserDTO);
-            await createdUser.save();
-            return createdUser;
-        }
-        catch (error) {
-            console.log("users.service | error:", error);
-        }
-    }
-
+  }
 }
